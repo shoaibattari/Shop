22dd3e84fff5bc246441499680d58795c40f66fc
shoaib memon <shoaibattari83@gmail.com>
2023-06-02 00:49:11 +0500

    sanity image show front end

---
+++ sanity/product.ts
@@ -0,0 +1,28 @@
+export const product = {
+    name: "product",
+    type: 'document',
+    title: "Product",
+    fields: [
+        {
+            name: "title",
+            title: 'Product Title',
+            type : "string"
+        },
+        {
+            name: "description",
+            title: 'Product Description',
+            type : "string"
+        } ,
+        {
+            name: "image",
+            title: 'Product Image',
+            type : "image"
+        } ,
+        {
+            name: "price",
+            title: 'Product Price',
+            type : "number"
+        } 
+    ]
+    
+}
