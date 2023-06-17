5d7fe218ef81912063d9a0e323c0a773a0982c82
shoaib memon <shoaibattari83@gmail.com>
2023-06-18 01:27:21 +0500

    link edit

--- src/app/classes/[class-wise-product]/page.tsx
+++ src/app/classes/[class-wise-product]/page.tsx
@@ -41,7 +41,7 @@ export default async function Home() {
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
-            <Link key={product.Slug} href={`/classes/${product._id}`}>
+            <Link key={product.Slug} href={`/products/${product._id}`}>
               <div className="group relative">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:scale-105 ">
                   <Image
